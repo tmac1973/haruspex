52808c0dad949c48d42db3e760f1cf3684fdd449
Tim MacDonald <tbaxmacdonald@gmail.com>
2026-03-28 16:02:09 -0500

    Fix espeak-rs linking: add sonic and pcaudiolib deps
    The kokoro-micro crate depends on espeak-rs-sys which needs libsonic
    and libpcaudio system libraries but doesn't link them in its build
    script. Added explicit link directives in our build.rs.

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
+    // espeak-rs-sys needs sonic and pcaudiolib for audio output
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rustc-link-lib=sonic");
+        println!("cargo:rustc-link-lib=pcaudio");
+    }
+
     tauri_build::build()
 }
